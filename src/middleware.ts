fdbce6ec6289cb8e2e3634ba97f7a7c20b674650
David Zechowy <davidzech27@gmail.com>
2023-04-02 11:04:34 -0700

    do not check for auth on /landing page

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,22 +2,29 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { authorizationCookieKey, getAuth } from "./server/auth/jwt";
 
-export const middleware = (request: NextRequest) => {
+export const middleware = async (request: NextRequest) => {
 	if (
 		request.nextUrl.pathname !== "/" &&
 		request.nextUrl.pathname !== "/signIn" &&
+		request.nextUrl.pathname !== "/landing" && // because authorization cookie isn't sent on redirect from oauthcallback
+		!request.nextUrl.pathname.startsWith("/api") &&
 		!request.nextUrl.pathname.startsWith("/_") // SUPER important
 	) {
 		const authorization = request.cookies.get(
 			authorizationCookieKey
 		)?.value;
 
-		if (!authorization || getAuth({ authorization }) === undefined)
+		if (
+			!authorization ||
+			(await getAuth({ authorization })) === undefined
+		) {
+			console.log("111111111", request.nextUrl.pathname, authorization);
 			return NextResponse.redirect(
 				new URL(
 					`${request.nextUrl.protocol}//${request.nextUrl.host}/signIn`
 				)
 			);
+		}
 	}
 
 	if (
