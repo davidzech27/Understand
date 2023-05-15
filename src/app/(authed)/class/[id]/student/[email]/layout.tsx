cf831e5e5f40a6cee4f31ced672805793c4ff410
David Zechowy <davidzech27@gmail.com>
2023-05-15 00:08:02 -0700

    add student page

---
+++ src/app/(authed)/class/[id]/student/[email]/layout.tsx
@@ -0,0 +1,80 @@
+import { notFound } from "next/navigation"
+import { cookies } from "next/headers"
+
+import colors from "~/colors.cjs"
+import Card from "~/components/Card"
+import User from "~/data/User"
+import { getAuthOrThrow } from "~/auth/jwt"
+import Avatar from "~/components/Avatar"
+
+interface Params {
+	id: string
+	email: string
+}
+
+export const generateMetadata = async ({
+	params: { email },
+}: {
+	params: Params
+}) => {
+	const name = (await User({ email }).get())?.name
+
+	return {
+		template: `%s | ${name}`,
+		default: name,
+	}
+}
+
+const StudentLayout = async ({
+	children,
+	params: { id, email },
+}: {
+	children: React.ReactNode
+	params: Params
+}) => {
+	email = decodeURIComponent(email)
+
+	const [student, role] = await Promise.all([
+		User({ email }).get(),
+		getAuthOrThrow({ cookies: cookies() }).then(({ email }) =>
+			User({ email }).courseRole({ id })
+		),
+	])
+
+	if (student === undefined || role !== "teacher") notFound()
+
+	return (
+		<div className="flex h-full flex-col space-y-2.5 overflow-y-scroll">
+			<Card className="flex items-center py-5 px-6">
+				<Avatar
+					src={student.photo}
+					name={student.name}
+					fallbackColor="primary"
+					className="h-20 w-20 rounded-full"
+				/>
+
+				<div className="ml-5 flex flex-col">
+					<span
+						style={{
+							background: `linear-gradient(to right, ${colors.primary}, ${colors.secondary})`,
+							WebkitBackgroundClip: "text",
+							backgroundClip: "text",
+							color: "transparent",
+						}}
+						className="mb-[1px] pb-1 text-3xl font-semibold leading-none opacity-90"
+					>
+						{student.name}
+					</span>
+
+					<span className="-mt-0.5 text-lg opacity-60">
+						{student.email}
+					</span>
+				</div>
+			</Card>
+
+			{children}
+		</div>
+	)
+}
+
+export default StudentLayout
---
+++ src/app/(authed)/class/[id]/student/[email]/page.tsx
@@ -0,0 +1,13 @@
+import Card from "~/components/Card"
+
+const StudentPage = () => {
+	return (
+		<Card className="flex flex-col justify-between py-5 px-6">
+			<span className="font-medium italic opacity-60">
+				Student insights coming soon...
+			</span>
+		</Card>
+	)
+}
+
+export default StudentPage
