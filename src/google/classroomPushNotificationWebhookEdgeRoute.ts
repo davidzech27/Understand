2eb3036478e97b6da9c884d1a1166d504746a973
David Zechowy <davidzech27@gmail.com>
2023-05-28 15:11:45 -0700

    print json in pub/sub webhook

--- src/google/classroomPushNotificationWebhookEdgeRoute.ts
+++ src/google/classroomPushNotificationWebhookEdgeRoute.ts
@@ -1,7 +1,7 @@
 import { type NextRequest } from "next/server"
 
 const webhookHandler = async (request: NextRequest) => {
-	console.log(JSON.stringify(request, null, 4))
+	console.log(JSON.stringify(await request.json(), null, 4))
 
 	return new Response()
 }
