2f6d795047b0da7d0955c3f375213580356fb7b4
David Zechowy <davidzech27@gmail.com>
2023-06-06 15:57:48 -0700

    don't use require in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
+import { dirname } from "path"
+import { fileURLToPath } from "url"
 // @ts-expect-error
-const nextBuildId = require("next-build-id")
+import nextBuildId from "next-build-id"
 
 import "./src/env.mjs"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 /** @type {import("next").NextConfig} */
 const config = {
