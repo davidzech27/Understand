ba8f571fac487eea83d361375320d076b2fe18ed
David Zechowy <davidzech27@gmail.com>
2023-08-25 10:54:17 -0700

    fix redirectTo in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,18 +2,24 @@ import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
 import { getAuth } from "~/auth/jwt"
+import redirectToCookieKey from "~/auth/redirectToCookieKey"
 
 export const config = {
 	matcher: ["/((?=home|class|assignment).+)"],
 }
 
 export const middleware = async (request: NextRequest) => {
-	if ((await getAuth({ cookies: request.cookies })) === undefined)
-		return NextResponse.redirect(
+	if ((await getAuth({ cookies: request.cookies })) === undefined) {
+		const response = NextResponse.redirect(
 			new URL(
 				`${request.nextUrl.protocol}//${request.nextUrl.host}/signIn`
 			)
 		)
+
+		response.cookies.set(redirectToCookieKey, request.url)
+
+		return response
+	}
 
 	return NextResponse.next()
 }
