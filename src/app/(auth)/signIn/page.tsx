4b431978dd5226c2bfe95c0600e1893993b5e9b2
David Zechowy <davidzech27@gmail.com>
2023-08-26 09:58:23 -0700

    add image and text for unverified google sign in

--- src/app/(auth)/signIn/page.tsx
+++ src/app/(auth)/signIn/page.tsx
@@ -23,10 +23,10 @@ export default function SignInPage() {
 					Understand
 				</div>
 
-				<div className="flex h-full w-full flex-col space-y-12 rounded-lg border-[0.5px] border-border bg-white p-6 shadow-xl shadow-[#00000016] mobile:justify-between mobile:space-y-0 mobile:pt-10">
+				<div className="flex h-full w-full flex-col space-y-12 rounded-lg border-[0.5px] border-border bg-white px-6 py-0 shadow-xl shadow-[#00000016] mobile:justify-between mobile:space-y-0 mobile:py-6 mobile:pt-10">
 					<div className="flex-[0.875] mobile:hidden" />
 
-					<h1 className="select-text text-center text-3xl font-semibold tracking-tight text-black/80 mobile:text-2xl">
+					<h1 className="select-text text-center text-2xl font-semibold tracking-tight text-black/80 mobile:text-xl">
 						Sign in with your Google account. Use your school Google
 						account if Understand has been approved by your district
 						(it probably hasn&apos;t yet).
@@ -42,13 +42,25 @@ export default function SignInPage() {
 						})}
 						passHref
 					>
-						<FancyButton
-							size="large"
-							className="sm-mobile:text-2xl"
-						>
+						<FancyButton size="large" className="mobile:text-2xl">
 							Sign in with Google
 						</FancyButton>
 					</Link>
+
+					<img
+						src="/UnverifiedGoogle.png"
+						alt="Screenshot of Google sign in screen"
+						className="mx-auto w-1/2 rounded-lg border-[0.5px] border-border object-cover shadow-lg shadow-[#00000016] duration-[400ms] ease-out hover:scale-105 lg-mobile:w-full"
+					/>
+
+					<div className="rounded-lg border-[0.5px] border-border bg-surface px-4 py-3">
+						<p className="select-text text-sm font-medium text-black/70">
+							Because we&apos;re currently awaiting approval for
+							Google verification, you may see this screen while
+							signing in. Click &quot;Advanced&quot;, then
+							&quot;Go to Understand (unsafe)&quot; to proceed.
+						</p>
+					</div>
 
 					<div className="flex-1 mobile:hidden" />
 				</div>
