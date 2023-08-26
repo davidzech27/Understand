d02bafe406d6f80a6bd62138fac09271093cd339
David Zechowy <davidzech27@gmail.com>
2023-08-26 13:09:46 -0700

    improve not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,11 @@
+import Link from "next/link"
+
 import env from "env.mjs"
 
 export const runtime = "edge"
 
 export const metadata = {
 	title: "Link not found | Understand",
-	description:
-		"The personalized educational content and student insight platform",
 }
 
 export default function NotFoundPage() {
@@ -17,10 +17,16 @@ export default function NotFoundPage() {
 		<div className="flex h-screen w-full flex-col items-center bg-gradient-to-tr from-primary to-secondary">
 			<div className="flex-[0.875]" />
 
-			<span className="cursor-default px-48 text-[5rem] font-semibold leading-none text-white">
+			<div className="flex cursor-default select-text flex-col items-center justify-center gap-12 whitespace-pre-wrap px-48 text-[5rem] font-extrabold tracking-tight text-white">
 				This link does not exist. If you think it should, please contact
-				us.
-			</span>
+				us.{"\n"}
+				<Link
+					href="/home"
+					className="block transition duration-200 hover:opacity-75 focus-visible:opacity-75 active:opacity-75"
+				>
+					Go home
+				</Link>
+			</div>
 
 			<div className="flex-1" />
 		</div>
